1336afdcfd89617e686768b2d075aaca455cbb07
MaxLSB <m.lasbordes@gmail.com>
2025-09-07 20:39:16 +0200

    Add GitHub Pages deployment configuration

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: "static",
+  site: "https://maxlsb.github.io",
+  base: "/new-portfolio",
   prefetch: true,
   compressHTML: true,
 });
