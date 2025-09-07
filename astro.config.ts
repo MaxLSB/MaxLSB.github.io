16d14f6b86c19577b26ef9541498cf1b44d436c7
MaxLSB <m.lasbordes@gmail.com>
2025-09-07 20:46:36 +0200

    Fix local development configuration

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: "static",
   site: "https://maxlsb.github.io",
-  base: "/new-portfolio",
+  base: process.env.NODE_ENV === 'development' ? '/' : "/new-portfolio",
   prefetch: true,
   compressHTML: true,
 });
